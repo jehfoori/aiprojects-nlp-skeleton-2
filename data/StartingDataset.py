a2aabeed761dd6833ca8ad7e0a2d313019399005
terru3 <terru3@hotmail.com>
2022-10-21 19:23:19 -0700

    made embeddings

--- data/StartingDataset.py
+++ data/StartingDataset.py
@@ -1,5 +1,6 @@
 import torch
 import pandas as pd
+import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 
 
@@ -9,18 +10,27 @@ class StartingDataset(torch.utils.data.Dataset):
     """
 
     # TODO: dataset constructor.
-    def __init__(self, data_path):
+    def __init__(self, data_path="/Users/Terru/Desktop/UCLA/ACM AI/Projects/train.csv"):
         '''
         data_path (str): path for the csv file that contains the data that you want to use
         '''
 
         # Preprocess the data. These are just library function calls so it's here for you
         self.df = pd.read_csv(data_path)
-        self.vectorizer = CountVectorizer(stop_words='english', max_df=0.99, min_df=0.005)
-        self.sequences = self.vectorizer.fit_transform(self.df.question_text.tolist()) # matrix of word counts for each sample
-        self.labels = self.df.target.tolist() # list of labels
-        self.token2idx = self.vectorizer.vocabulary_ # dictionary converting words to their counts
-        self.idx2token = {idx: token for token, idx in self.token2idx.items()} # same dictionary backwards
+
+        # Embeddings
+        self.words = []
+        self.word2idx = {}
+        self.embedding = {}
+        self.idx = 0
+
+        with open("/Users/Terru/Desktop/UCLA/ACM AI/Projects/glove.6B/glove.6B.300d.txt") as f:
+            for l in f:
+                line = l.split()
+                word = line[0]
+                vector = np.array([float(number) for number in line[1:]])
+                self.embedding[word] = vector
+                self.words.append(word)
 
     # TODO: return an instance from the dataset
     def __getitem__(self, i):
@@ -32,8 +42,10 @@ class StartingDataset(torch.utils.data.Dataset):
 
     # TODO: return the size of the dataset
     def __len__(self):
-        aman = 11302001
-        pigs_fly = False
-        if (pigs_fly):
-            return aman
         return self.sequences.shape[0]
+
+
+data = StartingDataset()
+print(data.df.head())
+print(len(data.df))
+print(data.embedding["the"])
